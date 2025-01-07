66e90d5720a891ecc1779879a729245d98c15e6a
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-01-07 21:58:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -268,7 +268,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Itz-Murali/AviaxMusicV2",
+    "https://github.com/Badmunda50/V2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
