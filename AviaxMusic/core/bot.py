52b1690cae22959cf7c43be14f19cd36fb886eb0
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-01-14 09:05:13 +0530

    Update bot.py

--- AviaxMusic/core/bot.py
+++ AviaxMusic/core/bot.py
@@ -1,97 +1,64 @@
-import uvloop
+import asyncio
+import importlib
 
-uvloop.install()
-
-from pyrogram import Client as PyrogramClient, errors
-from pyrogram.enums import ChatMemberStatus, ParseMode
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
 from telethon import TelegramClient
 
 import config
-from ..logging import LOGGER
+from AviaxMusic import LOGGER, app, userbot, Bad
+from AviaxMusic.core.call import Aviax
+from AviaxMusic.misc import sudo
+from AviaxMusic.plugins import ALL_MODULES
+from AviaxMusic.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
 
 
-class Aviax(PyrogramClient):
-    def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
-        super().__init__(
-            name="AviaxMusic",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            bot_token=config.BOT_TOKEN,
-            in_memory=True,
-            # parse_mode=ParseMode.HTML,
-            max_concurrent_transmissions=7,
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
+        exit()
+    sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("AviaxMusic.plugins" + all_module)
+    LOGGER("AviaxMusic.plugins").info("Successfully Imported Modules...")
+    await userbot.start()
+    await Aviax.start()
+    await Bad.start()  # Modified line
+    try:
+        await Aviax.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+    except NoActiveGroupCall:
+        LOGGER("AviaxMusic").error(
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
         )
-
-    async def start(self):
-        await super().start()
-
-        self.id = self.me.id
-        self.name = self.me.first_name + " " + (self.me.last_name or "")
-        self.username = self.me.username
-        self.mention = self.me.mention
-
-        try:
-            LOGGER(__name__).info(f"Attempting to send message to LOG_GROUP_ID: {config.LOG_GROUP_ID}")
-            await self.send_message(
-                chat_id=config.LOG_GROUP_ID,
-                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
-            )
-            LOGGER(__name__).info(f"Message sent successfully to LOG_GROUP_ID: {config.LOG_GROUP_ID}")
-        except (errors.ChannelInvalid, errors.PeerIdInvalid):
-            LOGGER(__name__).error(
-                "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
-            )
-            exit()
-        except Exception as ex:
-            LOGGER(__name__).error(
-                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}.\nException: {ex}"
-            )
-            exit()
-
-        a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
-        LOGGER(__name__).info(f"Bot member status in the group: {a.status}")
-        if a.status != ChatMemberStatus.ADMINISTRATOR:
-            LOGGER(__name__).error(
-                "Please promote your bot as an admin in your log group/channel."
-            )
-            exit()
-        LOGGER(__name__).info(f"Music Bot Started as {self.name}")
-
-    async def stop(self):
-        await super().stop()
+        exit()
+    except:
+        pass
+    await Aviax.decorators()
+    LOGGER("AviaxMusic").info(
+        "bot start")
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    await Bad.disconnect()
+    LOGGER("AviaxMusic").info("Stopping Aviax Music Bot...")
 
 
-class Bad(TelegramClient):
-    def __init__(self):
-        LOGGER(__name__).info(f"Starting Telethon Bot...")
-        super().__init__(
-            'telethon_session',
-            api_id=config.API_ID,
-            api_hash=config.API_HASH
-        )
-
-    async def start(self):
-        await super().start(bot_token=config.BOT_TOKEN)
-        me = await self.get_me()
-        self.id = me.id
-        self.name = me.first_name + " " + (me.last_name or "")
-        self.username = me.username
-        self.mention = f"@{self.username}"
-
-        try:
-            LOGGER(__name__).info(f"Attempting to send message to LOG_GROUP_ID: {config.LOG_GROUP_ID}")
-            await self.send_message(
-                config.LOG_GROUP_ID,
-                f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}"
-            )
-            LOGGER(__name__).info(f"Message sent successfully to LOG_GROUP_ID: {config.LOG_GROUP_ID}")
-        except Exception as ex:
-            LOGGER(__name__).error(
-                f"Telethon Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}.\nException: {ex}"
-            )
-            exit()
-        LOGGER(__name__).info(f"Telethon Bot Started as {self.name}")
-
-    async def stop(self):
-        await super().stop()
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())
