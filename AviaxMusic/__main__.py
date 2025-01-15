a47cd237a7e600a2eefcac79f9a1841eed414c0f
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-01-15 14:37:30 +0530

    Update __main__.py

--- AviaxMusic/__main__.py
+++ AviaxMusic/__main__.py
@@ -36,8 +36,12 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("AviaxMusic.plugins" + all_module)
-    LOGGER("AviaxMusic.plugins").info("Successfully Imported Modules...")
+        imported_module = importlib.import_module(all_module)
+
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
+    LOGGER("AviaxMusic.plugins").info("Successfully Imported All Modules ")
     await userbot.start()
     await Aviax.start()
     await Bad.start()
@@ -61,3 +65,4 @@ async def init():
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(init())
+    
