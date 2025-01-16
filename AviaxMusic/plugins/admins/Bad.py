e6af6e5fb1473bdd194e0dc12e3008a7d8393479
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-01-16 21:17:19 +0530

    Update Bad.py

--- AviaxMusic/plugins/admins/Bad.py
+++ AviaxMusic/plugins/admins/Bad.py
@@ -48,7 +48,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["roken"], prefixes=["b", "B"]) & admin_filter)
+@app.on_message(filters.command(["obo"], prefixes=["r", "R"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
