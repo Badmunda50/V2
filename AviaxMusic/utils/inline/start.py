11c2dda21dec0ab3dea88c0f27586f14a8c88679
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-01-20 18:39:37 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -29,7 +29,7 @@ def private_panel(OWNER: Union[bool, int] = None):
         ], 
         [
             InlineKeyboardButton(
-                text="Hᴇʟᴘ & Cᴏᴍᴍᴀɴᴅs", callback_data="feature"
+                text="Hᴇʟᴘ & Cᴏᴍᴍᴀɴᴅs", callback_data="gotohelp"
             ),
         ],
       [
@@ -51,4 +51,4 @@ def private_panel(OWNER: Union[bool, int] = None):
     
 ]
     return buttons
-
+    
