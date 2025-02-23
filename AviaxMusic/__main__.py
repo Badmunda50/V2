13415155f603b6c03585941d9f1974896c7f1ae5
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-02-23 21:47:45 +0530

    Update __main__.py

--- AviaxMusic/__main__.py
+++ AviaxMusic/__main__.py
@@ -6,8 +6,7 @@ from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from AviaxMusic import LOGGER, HELPABLE, app, userbot, Bad
-from AviaxMusic.core.bot import application
+from AviaxMusic import LOGGER, HELPABLE, app, userbot, Bad, application
 from AviaxMusic.core.call import Aviax
 from AviaxMusic.misc import sudo
 from AviaxMusic.plugins import ALL_MODULES
