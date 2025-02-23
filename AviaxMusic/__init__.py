7ce16c89733741e04d755be5fdaf30da4c57edf7
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-02-23 21:58:08 +0530

    Update __init__.py

--- AviaxMusic/__init__.py
+++ AviaxMusic/__init__.py
@@ -2,7 +2,7 @@ import config
 import pytz
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-from AviaxMusic.core.bot import Aviax, Bad
+from AviaxMusic.core.bot import Aviax, Bad application
 from AviaxMusic.core.dir import dirr
 from AviaxMusic.core.git import git
 from AviaxMusic.core.userbot import Userbot
@@ -21,6 +21,7 @@ heroku()
 
 app = Aviax()
 Bad = Bad()
+application = application()
 userbot = Userbot()
 
 from .platforms import *
