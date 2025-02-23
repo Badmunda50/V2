e8a1e31ebd886876b2cc14eb6a5eb132534ca2e9
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-02-23 20:22:17 +0530

    Update __main__.py

--- AviaxMusic/__main__.py
+++ AviaxMusic/__main__.py
@@ -2,7 +2,6 @@ import asyncio
 import importlib
 
 from pyrogram import idle
-from telegram.ext import Application
 from pytgcalls.exceptions import NoActiveGroupCall
 from telethon import TelegramClient
 
