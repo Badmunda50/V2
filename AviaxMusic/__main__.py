b240d08557ec7965b3f4f9326ba39a4565f478ef
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-02-23 21:43:24 +0530

    Update __main__.py

--- AviaxMusic/__main__.py
+++ AviaxMusic/__main__.py
@@ -68,4 +68,4 @@ async def init():
     LOGGER("AviaxMusic").info("Stopping Aviax Music Bot...")
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.get_event_loop().run_until_complete(init())
