c05d23d3e2d7843a9febefd397a66e297da50f97
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-02-26 15:56:46 +0530

    Update __main__.py

--- AviaxMusic/__main__.py
+++ AviaxMusic/__main__.py
@@ -1,19 +1,17 @@
 import asyncio
 import importlib
 
-import nest_asyncio
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from AviaxMusic import LOGGER, HELPABLE, app, userbot, Bad, application
+from AviaxMusic import LOGGER, app, userbot
 from AviaxMusic.core.call import Aviax
 from AviaxMusic.misc import sudo
 from AviaxMusic.plugins import ALL_MODULES
 from AviaxMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
-nest_asyncio.apply()
 
 async def init():
     if (
@@ -37,34 +35,28 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        imported_module = importlib.import_module(all_module)
-
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
-                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
-    LOGGER("AviaxMusic.plugins").info("Successfully Imported All Modules ")
-    await Aviax.start()
-    await Bad.start()
+        importlib.import_module("AviaxMusic.plugins" + all_module)
+    LOGGER("AviaxMusic.plugins").info("Successfully Imported Modules...")
     await userbot.start()
-    await application.run_polling()
-    await application.start()
+    await Aviax.start()
     try:
         await Aviax.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("AviaxMusic").error(
-            "Please turn on the videochat of your log group/channel.\n\nStopping Bot..."
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
         )
         exit()
     except:
         pass
     await Aviax.decorators()
-    LOGGER("AviaxMusic").info("bot start")
+    LOGGER("AviaxMusic").info(
+        "\x41\x76\x69\x61\x78\x20\x4d\x75\x73\x69\x63\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x0a\x0a\x44\x6f\x6e\x27\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x41\x76\x69\x61\x78\x4f\x66\x66\x69\x63\x69\x61\x6c"
+    )
     await idle()
     await app.stop()
-    await Bad.disconnect()
-    await application.shutdown()  # Call shutdown method on the instance
     await userbot.stop()
     LOGGER("AviaxMusic").info("Stopping Aviax Music Bot...")
+
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(init())
